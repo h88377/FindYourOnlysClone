7a343cefdce2d16f05d429b7c0cb2c8c7123b6c9
Wayne <h88377@gmail.com>
2023-04-13 17:26:24 +0800

    Initial commit

---
+++ FindYourOnlysClone/FindYourOnlysClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindYourOnlysClone
+//
+//  Created by 鄭昭韋 on 2023/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
