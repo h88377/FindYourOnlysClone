9c43a6088c7761bd0f019ae2909045b2a1957241
Wayne <h88377@gmail.com>
2023-04-14 20:58:45 +0800

    Move `AdoptListViewModel` to its own file

--- FindYourOnlysClone/FindYourOnlysClone/Pet UI/AdoptListViewController.swift
+++ FindYourOnlysClone/FindYourOnlysClone/Pet UI/AdoptListViewController.swift
@@ -7,45 +7,6 @@
 
 import UIKit
 
-class AdoptListViewModel {
-    typealias Observer<T> = ((T) -> Void)
-    
-    private var petLoader: PetLoader?
-    
-    init(petLoader: PetLoader) {
-        self.petLoader = petLoader
-    }
-    
-    private var currentPage = 0
-    var isPetLoadingStateOnChange: Observer<Bool>?
-    var isPetsAppendingStateOnChange: Observer<[Pet]>?
-    var isPetsRefreshingStateOnChange: Observer<[Pet]>?
-    
-    func refreshPets() {
-        currentPage = 0
-        loadPets()
-    }
-    
-    func loadNextPagePets() {
-        currentPage += 1
-        loadPets()
-    }
-    
-    private func loadPets() {
-        isPetLoadingStateOnChange?(true)
-        petLoader?.load(with: AdoptPetRequest(page: currentPage)) { [weak self] result in
-            if let pets = try? result.get() {
-                if self?.currentPage == 0 {
-                    self?.isPetsRefreshingStateOnChange?(pets)
-                } else {
-                    self?.isPetsAppendingStateOnChange?(pets)
-                }
-            }
-            self?.isPetLoadingStateOnChange?(false)
-        }
-    }
-}
-
 class AdoptListViewController: UICollectionViewController {
     private lazy var dataSource: UICollectionViewDiffableDataSource<Int, Pet> = {
         .init(collectionView: collectionView) { [weak self] collectionView, indexPath, pet in
---
+++ FindYourOnlysClone/FindYourOnlysClone/Pet UI/AdoptListViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  AdoptListViewModel.swift
+//  FindYourOnlysClone
+//
+//  Created by 鄭昭韋 on 2023/4/14.
+//
+
+import Foundation
+
+class AdoptListViewModel {
+    typealias Observer<T> = ((T) -> Void)
+    
+    private var petLoader: PetLoader?
+    
+    init(petLoader: PetLoader) {
+        self.petLoader = petLoader
+    }
+    
+    private var currentPage = 0
+    
+    var isPetLoadingStateOnChange: Observer<Bool>?
+    var isPetsAppendingStateOnChange: Observer<[Pet]>?
+    var isPetsRefreshingStateOnChange: Observer<[Pet]>?
+    
+    func refreshPets() {
+        currentPage = 0
+        loadPets()
+    }
+    
+    func loadNextPagePets() {
+        currentPage += 1
+        loadPets()
+    }
+    
+    private func loadPets() {
+        isPetLoadingStateOnChange?(true)
+        petLoader?.load(with: AdoptPetRequest(page: currentPage)) { [weak self] result in
+            if let pets = try? result.get() {
+                if self?.currentPage == 0 {
+                    self?.isPetsRefreshingStateOnChange?(pets)
+                } else {
+                    self?.isPetsAppendingStateOnChange?(pets)
+                }
+            }
+            self?.isPetLoadingStateOnChange?(false)
+        }
+    }
+}
