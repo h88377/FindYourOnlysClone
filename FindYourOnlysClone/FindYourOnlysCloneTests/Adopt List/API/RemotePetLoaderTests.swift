3832ed2a066e30e484eaf80bfc09d4fc10340e44
Wayne <h88377@gmail.com>
2023-04-17 04:31:35 +0800

    `load(with:)` requests data from url request

--- FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/API/RemotePetLoaderTests.swift
+++ FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/API/RemotePetLoaderTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import FindYourOnlysClone
 
 protocol HTTPClient {
-    func dispatch()
+    func dispatch(_ request: URLRequest)
 }
 
 final class RemotePetLoader {
@@ -19,6 +19,20 @@ final class RemotePetLoader {
     init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
+    }
+    
+    func load(with request: AdoptListRequest) {
+        var component = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        component?.queryItems = [
+            URLQueryItem(name: "UnitId", value: "QcbUEzN6E6DL"),
+            URLQueryItem(name: "$top", value: "20"),
+            URLQueryItem(name: "$skip", value: "\(20 * request.page)"),
+        ]
+        
+        let enrichedURL = component?.url ?? url
+        let request = URLRequest(url: enrichedURL)
+        
+        client.dispatch(request)
     }
 }
 
@@ -32,11 +46,23 @@ class RemotePetLoaderTests: XCTestCase {
         XCTAssertTrue(client.receivedURLs.isEmpty)
     }
     
+    func test_loadWithRequest_requestsDataFromRequest() {
+        let page = 0
+        let url = URL(string: "https://any-url.com")!
+        let expectedURL = URL(string: "https://any-url.com?UnitId=QcbUEzN6E6DL&$top=20&$skip=\(20 * page)")!
+        let client = HTTPClientSpy()
+        let sut = RemotePetLoader(url: url, client: client)
+        
+        sut.load(with: AdoptListRequest(page: page))
+        
+        XCTAssertEqual(client.receivedURLs, [expectedURL])
+    }
+    
     private class HTTPClientSpy: HTTPClient {
         private(set) var receivedURLs = [URL]()
         
-        func dispatch() {
-            
+        func dispatch(_ request: URLRequest) {
+            receivedURLs.append(request.url!)
         }
     }
 }
