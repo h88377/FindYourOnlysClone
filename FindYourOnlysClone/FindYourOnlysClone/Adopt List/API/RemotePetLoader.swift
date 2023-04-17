61d07e14b59ca1c558fdb92aed272603254c757e
Wayne <h88377@gmail.com>
2023-04-17 16:34:31 +0800

    Move `PetsMapper` to its own file

--- FindYourOnlysClone/FindYourOnlysClone/Adopt List/API/RemotePetLoader.swift
+++ FindYourOnlysClone/FindYourOnlysClone/Adopt List/API/RemotePetLoader.swift
@@ -59,29 +59,6 @@ final class RemotePetLoader {
     }
 }
 
-private final class PetsMapper {
-    private init() {}
-    
-    private static var OK_200: Int { return 200 }
-    
-    private static let formatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        return formatter
-    }()
-    
-    static func map(with data: Data, _ response: HTTPURLResponse) throws -> [RemotePet] {
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(formatter)
-        
-        guard response.statusCode == OK_200, let remotePets = try? decoder.decode([RemotePet].self, from: data) else {
-            throw RemotePetLoader.Error.invalidData
-        }
-        
-        return remotePets
-    }
-}
-
 private extension Array where Element == RemotePet {
     func toModels() -> [Pet] {
         return map { Pet(id: $0.id, location: $0.location, kind: $0.kind, gender: $0.gender, bodyType: $0.bodyType, color: $0.color, age: $0.age, sterilization: $0.sterilization, bacterin: $0.bacterin, foundPlace: $0.foundPlace, status: $0.status, remark: $0.remark, openDate: $0.openDate, closedDate: $0.closedDate, updatedDate: $0.updatedDate, createdDate: $0.createdDate, photoURL: $0.photoURL, address: $0.address, telephone: $0.telephone, variety: $0.variety, shelterName: $0.shelterName) }
