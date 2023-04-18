7e12869619df4887353d17668fa9ea50733c004d
Wayne <h88377@gmail.com>
2023-04-18 17:39:38 +0800

    Create a private serial queue to perform `URLProtocolStub` global data access (read, write) synchrously to avoid data races

--- FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/API/URLSessionHTTPClientTests.swift
+++ FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/API/URLSessionHTTPClientTests.swift
@@ -162,18 +162,23 @@ class URLSessionHTTPClientTests: XCTestCase {
             let data: Data?
             let response: URLResponse?
             let error: Error?
+            let requestObserverHandler: ((URLRequest) -> Void)?
         }
         
-        private static var requestObserverHandler: ((URLRequest) -> Void)?
+        private static var _stub: Stub?
+        private static var stub: Stub? {
+            get { queue.sync { return _stub } }
+            set { queue.sync { _stub = newValue } }
+        }
         
-        private static var stub: Stub?
+        private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
 
         static func observeRequest(_ observer: @escaping (URLRequest) -> Void) {
-            requestObserverHandler = observer
+            stub = Stub(data: nil, response: nil, error: nil, requestObserverHandler: observer)
         }
         
         static func stub(data: Data?, response: URLResponse?, error: Error?) {
-            stub = Stub(data: data, response: response, error: error)
+            stub = Stub(data: data, response: response, error: error, requestObserverHandler: nil)
         }
         
         static func startInterceptingRequest() {
@@ -182,7 +187,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         static func stopInterceptingRequest() {
             stub = nil
-            requestObserverHandler = nil
             URLProtocol.unregisterClass(URLProtocolStub.self)
         }
         
@@ -208,11 +212,9 @@ class URLSessionHTTPClientTests: XCTestCase {
             }
             
             client?.urlProtocolDidFinishLoading(self)
-            URLProtocolStub.requestObserverHandler?(request)
+            URLProtocolStub.stub?.requestObserverHandler?(request)
         }
         
-        override func stopLoading() {
-            
-        }
+        override func stopLoading() {}
     }
 }
