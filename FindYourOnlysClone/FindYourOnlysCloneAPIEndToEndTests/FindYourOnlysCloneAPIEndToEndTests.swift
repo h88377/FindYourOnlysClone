f055cc45574892372620595b7d4ca07040564809
Wayne <h88377@gmail.com>
2023-04-19 03:44:58 +0800

    End to end test server get pets result match fixed pets count

--- FindYourOnlysClone/FindYourOnlysCloneAPIEndToEndTests/FindYourOnlysCloneAPIEndToEndTests.swift
+++ FindYourOnlysClone/FindYourOnlysCloneAPIEndToEndTests/FindYourOnlysCloneAPIEndToEndTests.swift
@@ -6,7 +6,27 @@
 //
 
 import XCTest
+@testable import FindYourOnlysClone
 
 final class FindYourOnlysCloneAPIEndToEndTests: XCTestCase {
     
+    func test_endToEndTestServerGetPetsReuslt_matchesFixedPetsCount() {
+        let url = URL(string: "https://data.coa.gov.tw/Service/OpenData/TransService.aspx")!
+        let client = URLSessionHTTPClient()
+        let sut = RemotePetLoader(baseURL: url, client: client)
+        let exp = expectation(description: "Wait for result")
+        
+        sut.load(with: AdoptListRequest(page: 0)) { result in
+            switch result {
+            case let .success(pets):
+                XCTAssertEqual(pets.count, 20)
+                
+            default:
+                XCTFail("Expected to succeed with 20 pets data, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 3.0)
+    }
 }
