07aa15d9a825974c0f0746bfec8b5ce1e4f2a580
Wayne <h88377@gmail.com>
2023-04-19 12:10:06 +0800

    Remove unused parameter in the composition layer

--- FindYourOnlysClone/FindYourOnlysClone/AdoptListUIComposer.swift
+++ FindYourOnlysClone/FindYourOnlysClone/AdoptListUIComposer.swift
@@ -15,19 +15,19 @@ final class AdoptListUIComposer {
         let controller = AdoptListViewController(viewModel: viewModel)
         
         viewModel.isPetsRefreshingStateOnChange = { [weak controller] pets in
-            let cellControllers = adaptPetsToCellControllersWith(pets, petLoader: petLoader, imageLoader: imageLoader)
+            let cellControllers = adaptPetsToCellControllersWith(pets, imageLoader: imageLoader)
             controller?.set(cellControllers)
         }
         
         viewModel.isPetsAppendingStateOnChange = { [weak controller] pets in
-            let cellControllers = adaptPetsToCellControllersWith(pets, petLoader: petLoader, imageLoader: imageLoader)
+            let cellControllers = adaptPetsToCellControllersWith(pets, imageLoader: imageLoader)
             controller?.append(cellControllers)
         }
         
         return controller
     }
     
-    private static func adaptPetsToCellControllersWith(_ pets: [Pet], petLoader: PetLoader, imageLoader: PetImageDataLoader) -> [AdoptListCellViewController] {
+    private static func adaptPetsToCellControllersWith(_ pets: [Pet], imageLoader: PetImageDataLoader) -> [AdoptListCellViewController] {
         return pets.map { pet in
             let cellViewModel = AdoptListCellViewModel(pet: pet, imageLoader: imageLoader, imageTransformer: UIImage.init)
             let cellController = AdoptListCellViewController(viewModel: cellViewModel)
