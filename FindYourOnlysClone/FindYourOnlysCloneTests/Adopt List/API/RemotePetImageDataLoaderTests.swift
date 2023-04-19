2ad4ecf91bdc083ffe150acae1dd2083015e8cde
Wayne <h88377@gmail.com>
2023-04-19 17:20:44 +0800

    does not request image data from url

--- FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/API/RemotePetImageDataLoaderTests.swift
+++ FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/API/RemotePetImageDataLoaderTests.swift
@@ -6,7 +6,37 @@
 //
 
 import XCTest
+@testable import FindYourOnlysClone
+
+final class RemotePetImageDataLoader {
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL) {
+        
+    }
+}
 
 class RemotePetImageDataLoaderTests: XCTestCase {
     
+    func test_init_doesNotRequestImageDataFromURL() {
+        let client = HTTPClientSpy()
+        _ = RemotePetImageDataLoader(client: client)
+        
+        XCTAssertEqual(client.receivedURLs, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private class HTTPClientSpy: HTTPClient {
+        private(set) var receivedURLs = [URL]()
+        
+        func dispatch(_ request: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) {
+            
+        }
+        
+    }
 }
