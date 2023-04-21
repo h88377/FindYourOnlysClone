1e978f8a0ca2a9d81d88dcde343bc5057c329f92
Wayne <h88377@gmail.com>
2023-04-21 17:52:03 +0800

    Make `petImageView` of `AdoptListCell` to nil when invoking `requestPetImageData` to avoid cells displaying unmatched image when scrolling collection view

--- FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/Controller/AdoptListCellViewController.swift
+++ FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/Controller/AdoptListCellViewController.swift
@@ -41,6 +41,8 @@ final class AdoptListCellViewController {
     }
     
     func requestPetImageData() {
+        cell?.petImageView.image = nil
+        
         viewModel.loadPetImageData()
     }
     
