26a34ef1059c70ddb973d062f9a7a8a8147a7ad6
Wayne <h88377@gmail.com>
2023-04-24 01:54:19 +0800

    Move `AdoptListPaginationViewController` to its own file

--- FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/Controller/AdoptListViewController.swift
+++ FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/Controller/AdoptListViewController.swift
@@ -37,39 +37,6 @@ final class AdoptListPaginationViewModel {
     
 }
 
-final class AdoptListPaginationViewController {
-    private let viewModel: AdoptListPaginationViewModel
-    
-    init(viewModel: AdoptListPaginationViewModel) {
-        self.viewModel = viewModel
-        self.setUpBinding()
-    }
-    
-    private var isPaginating = false
-    
-    func resetPage() {
-        viewModel.resetPage()
-    }
-     
-    func paginate(on scrollView: UIScrollView) {
-        guard scrollView.isDragging, !isPaginating else { return }
-        
-        let offsetY = scrollView.contentOffset.y
-        let contentHeight = scrollView.contentSize.height
-        let frameHeight = scrollView.frame.height
-        
-        if offsetY > (contentHeight - frameHeight) {
-            viewModel.loadNextPage()
-        }
-    }
-    
-    private func setUpBinding() {
-        viewModel.isPetPaginationLoadingStateOnChange = { [weak self] isPaginating in
-            self?.isPaginating = isPaginating
-        }
-    }
-}
-
 final class AdoptListViewController: UICollectionViewController {
     private lazy var dataSource: UICollectionViewDiffableDataSource<Int, AdoptListCellViewController> = {
         .init(collectionView: collectionView) { [weak self] collectionView, indexPath, controller in
