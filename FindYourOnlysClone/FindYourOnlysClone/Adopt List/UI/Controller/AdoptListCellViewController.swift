8cfd0a8bf13216b887293dea793fc0785923a440
Wayne <h88377@gmail.com>
2023-04-24 13:26:25 +0800

    Move UIColor extension to its own file

--- FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/Controller/AdoptListCellViewController.swift
+++ FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/Controller/AdoptListCellViewController.swift
@@ -76,30 +76,3 @@ extension AdoptListCellViewController: Hashable {
       hasher.combine(id)
     }
 }
-
-extension UIColor {
-    static let maleColor = hexStringToUIColor(hex: "398AB9")
-    static let femaleColor = hexStringToUIColor(hex: "ffcb65")
-    static let projectIconColor = hexStringToUIColor(hex: "578c93")
-    
-    static func hexStringToUIColor(hex: String) -> UIColor {
-        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        
-        if (cString.hasPrefix("#")) {
-            cString.remove(at: cString.startIndex)
-        }
-        
-        if ((cString.count) != 6) {
-            return UIColor.gray
-        }
-        
-        var rgbValue:UInt64 = 0
-        Scanner(string: cString).scanHexInt64(&rgbValue)
-        
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0))
-    }
-}
