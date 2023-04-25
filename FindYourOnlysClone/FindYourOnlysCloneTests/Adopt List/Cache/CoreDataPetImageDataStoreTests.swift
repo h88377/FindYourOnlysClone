dad796728387c912e203183dbe24ca903b5feadb
Wayne <h88377@gmail.com>
2023-04-25 19:37:26 +0800

    Create empty `CoreDataPetImageDataStoreTests` file

---
+++ FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/Cache/CoreDataPetImageDataStoreTests.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataPetImageDataStoreTests.swift
+//  FindYourOnlysCloneTests
+//
+//  Created by 鄭昭韋 on 2023/4/25.
+//
+
+import XCTest
+
+class CoreDataPetImageDataStoreTests: XCTestCase {
+    
+    
+}
