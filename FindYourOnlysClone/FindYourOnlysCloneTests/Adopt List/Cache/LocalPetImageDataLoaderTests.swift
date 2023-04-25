b8491c330c575ed2548543622e59336df25b8ca4
Wayne <h88377@gmail.com>
2023-04-25 14:48:10 +0800

    `LocalPetImageDataLoader` does not request image data upon creation

--- FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/Cache/LocalPetImageDataLoaderTests.swift
+++ FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/Cache/LocalPetImageDataLoaderTests.swift
@@ -7,6 +7,34 @@
 
 import XCTest
 
+protocol PetStore {
+    
+}
+
+final class LocalPetImageDataLoader {
+    private let store: PetStore
+    
+    init(store: PetStore) {
+        self.store = store
+    }
+}
+
 class LocalPetImageDataLoaderTests: XCTestCase {
     
+    func test_init_doesNotRequestImageDataUponCreation() {
+        let store = PetStoreSpy()
+        _ = LocalPetImageDataLoader(store: store)
+        
+        XCTAssertEqual(store.loadCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private class PetStoreSpy: PetStore {
+        private(set) var loadCallCount = 0
+        
+        func loadImageData() {
+            
+        }
+    }
 }
