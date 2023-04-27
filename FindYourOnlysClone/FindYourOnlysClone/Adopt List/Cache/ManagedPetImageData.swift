fe54c36ea4cc081624ae9c2b6b38a8ccf75da253
Wayne <h88377@gmail.com>
2023-04-27 14:22:52 +0800

    Create `ManagedPetImageData` managed object class

---
+++ FindYourOnlysClone/FindYourOnlysClone/Adopt List/Cache/ManagedPetImageData.swift
@@ -0,0 +1,14 @@
+//
+//  ManagedPetImageData.swift
+//  FindYourOnlysClone
+//
+//  Created by 鄭昭韋 on 2023/4/27.
+//
+
+import CoreData
+
+class ManagedPetImageData: NSManagedObject {
+    @NSManaged var url: URL
+    @NSManaged var timestamp: Date
+    @NSManaged var value: Data
+}
