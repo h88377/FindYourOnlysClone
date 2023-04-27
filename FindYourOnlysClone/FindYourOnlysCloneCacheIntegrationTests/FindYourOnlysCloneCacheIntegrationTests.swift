43395c1ca108d1d54112ed9c03cdeefe6c92487a
Wayne <h88377@gmail.com>
2023-04-27 18:12:24 +0800

    Create empty `FindYourOnlysCloneCacheIntegrationTests` and include in the `CI` scheme

---
+++ FindYourOnlysClone/FindYourOnlysCloneCacheIntegrationTests/FindYourOnlysCloneCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  FindYourOnlysCloneCacheIntegrationTests.swift
+//  FindYourOnlysCloneCacheIntegrationTests
+//
+//  Created by 鄭昭韋 on 2023/4/27.
+//
+
+import XCTest
+
+final class FindYourOnlysCloneCacheIntegrationTests: XCTestCase {
+
+}
