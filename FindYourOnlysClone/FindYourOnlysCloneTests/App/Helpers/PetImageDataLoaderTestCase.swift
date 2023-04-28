4303e52e9d2449188b442fabfb48822122d0b224
Wayne <h88377@gmail.com>
2023-04-28 17:56:10 +0800

    Move `PetImageDataLoaderTestCase` into its own file and make `PetImageDataLoaderWithFallbackCompositeTests` conform to it

---
+++ FindYourOnlysClone/FindYourOnlysCloneTests/App/Helpers/PetImageDataLoaderTestCase.swift
@@ -0,0 +1,34 @@
+//
+//  PetImageDataLoaderTestCase.swift
+//  FindYourOnlysCloneTests
+//
+//  Created by 鄭昭韋 on 2023/4/28.
+//
+
+import XCTest
+@testable import FindYourOnlysClone
+
+protocol PetImageDataLoaderTestCase: XCTestCase {}
+
+extension PetImageDataLoaderTestCase {
+    func expect(_ sut: PetImageDataLoader, toCompleteWith expectedResult: PetImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for completion")
+        _ = sut.loadImageData(from: anyURL()) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData, "Expected \(expectedData), got \(receivedData) instead", file: file, line: line)
+                
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, "Expected \(expectedError), got \(receivedError) instead", file: file, line: line)
+                
+            default:
+                XCTFail("Expected to receive \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+}
--- FindYourOnlysClone/FindYourOnlysCloneTests/App/PetImageDataLoaderWithFallbackCompositeTests.swift
+++ FindYourOnlysClone/FindYourOnlysCloneTests/App/PetImageDataLoaderWithFallbackCompositeTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import FindYourOnlysClone
 
-class PetImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+class PetImageDataLoaderWithFallbackCompositeTests: XCTestCase, PetImageDataLoaderTestCase {
     
     func test_loadImageData_deliversPrimarySuccessfulResultOnPrimarySuccess() {
         let primaryData = anyData()
@@ -78,51 +78,5 @@ class PetImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         trackForMemoryLeak(primaryLoader, file: file, line: line)
         trackForMemoryLeak(fallbackLoader, file: file, line: line)
         return (sut, primaryLoader, fallbackLoader)
-    }
-    
-    private func expect(_ sut: PetImageDataLoader, toCompleteWith expectedResult: PetImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        let exp = expectation(description: "Wait for completion")
-        _ = sut.loadImageData(from: anyURL()) { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedData), .success(expectedData)):
-                XCTAssertEqual(receivedData, expectedData, "Expected \(expectedData), got \(receivedData) instead", file: file, line: line)
-                
-            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
-                XCTAssertEqual(receivedError, expectedError, "Expected \(expectedError), got \(receivedError) instead", file: file, line: line)
-                
-            default:
-                XCTFail("Expected to receive \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            exp.fulfill()
-        }
-        
-        action()
-        
-        wait(for: [exp], timeout: 1.0)
-    }
-}
-
-protocol PetImageDataLoaderTestCase: XCTestCase {}
-
-extension PetImageDataLoaderTestCase {
-    func expect(_ sut: PetImageDataLoader, toCompleteWith expectedResult: PetImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        let exp = expectation(description: "Wait for completion")
-        _ = sut.loadImageData(from: anyURL()) { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedData), .success(expectedData)):
-                XCTAssertEqual(receivedData, expectedData, "Expected \(expectedData), got \(receivedData) instead", file: file, line: line)
-                
-            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
-                XCTAssertEqual(receivedError, expectedError, "Expected \(expectedError), got \(receivedError) instead", file: file, line: line)
-                
-            default:
-                XCTFail("Expected to receive \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            exp.fulfill()
-        }
-        
-        action()
-        
-        wait(for: [exp], timeout: 1.0)
     }
 }
