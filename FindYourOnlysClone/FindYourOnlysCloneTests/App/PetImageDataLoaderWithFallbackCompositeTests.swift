7e075223eca35097fa5c04490749070b40949dc5
Wayne <h88377@gmail.com>
2023-04-28 15:11:07 +0800

    Create empty `PetImageDataLoaderWithFallbackCompositeTests`

---
+++ FindYourOnlysClone/FindYourOnlysCloneTests/App/PetImageDataLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,12 @@
+//
+//  PetImageDataLoaderWithFallbackCompositeTests.swift
+//  FindYourOnlysCloneTests
+//
+//  Created by 鄭昭韋 on 2023/4/28.
+//
+
+import XCTest
+
+class PetImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+    
+}
