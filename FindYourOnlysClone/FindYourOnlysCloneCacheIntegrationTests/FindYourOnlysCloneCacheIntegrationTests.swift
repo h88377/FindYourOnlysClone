403bda7e98d4f44b9f30389d9bec87e074902d86
Wayne <h88377@gmail.com>
2023-04-28 11:31:59 +0800

    `LocalPetImageDataLoader.load` delivers saved image data from a separate instance since the instance performs save operation and the instance performs load operation maybe different

--- FindYourOnlysClone/FindYourOnlysCloneCacheIntegrationTests/FindYourOnlysCloneCacheIntegrationTests.swift
+++ FindYourOnlysClone/FindYourOnlysCloneCacheIntegrationTests/FindYourOnlysCloneCacheIntegrationTests.swift
@@ -6,7 +6,67 @@
 //
 
 import XCTest
+@testable import FindYourOnlysClone
 
 final class FindYourOnlysCloneCacheIntegrationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        setUpEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        cleanStoreSideEffects()
+    }
+    
+    func test_loadImageData_deliversSavedDataOnASeparatedInstance() {
+        let imageData = anyData()
+        let imageURL = anyURL()
+        let loaderToSave = makeSUT()
+        let loaderToLoad = makeSUT()
+        
+        loaderToSave.save(data: imageData, for: imageURL) { result in
+            switch result {
+            case .success: break
+            default: XCTFail()
+            }
+        }
 
+        _ = loaderToLoad.loadImageData(from: imageURL) { result in
+            switch result {
+            case let .success(data):
+                XCTAssertEqual(imageData, data)
+            default: XCTFail()
+            }
+        }
+        
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> LocalPetImageDataLoader {
+        let bundle = Bundle(for: CoreDataPetImageDataStore.self)
+        let testSpecificStoreURL = testSpecificStoreURL()
+        let store = try! CoreDataPetImageDataStore(bundle: bundle, storeURL: testSpecificStoreURL)
+        let sut = LocalPetImageDataLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeak(store, file: file, line: line)
+        trackForMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func testSpecificStoreURL() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathExtension("\(type(of: self)).sqlite")
+    }
+    
+    private func setUpEmptyStoreState() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+    
+    private func cleanStoreSideEffects() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+    
 }
