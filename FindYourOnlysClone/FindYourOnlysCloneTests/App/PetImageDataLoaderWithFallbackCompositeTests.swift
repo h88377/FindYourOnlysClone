7a5843603acd0dfa69971acc091a1276a6b1fa78
Wayne <h88377@gmail.com>
2023-04-28 17:53:18 +0800

    Extract reusable helper method into a protocol `PetImageDataLoaderTestCase` extension method and constrain the conformance of this protocol should be a `XCTestCase`

--- FindYourOnlysClone/FindYourOnlysCloneTests/App/PetImageDataLoaderWithFallbackCompositeTests.swift
+++ FindYourOnlysClone/FindYourOnlysCloneTests/App/PetImageDataLoaderWithFallbackCompositeTests.swift
@@ -80,7 +80,32 @@ class PetImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         return (sut, primaryLoader, fallbackLoader)
     }
     
-    private func expect(_ sut: PetImageDataLoaderWithFallbackComposite, toCompleteWith expectedResult: PetImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+    private func expect(_ sut: PetImageDataLoader, toCompleteWith expectedResult: PetImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for completion")
+        _ = sut.loadImageData(from: anyURL()) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData, "Expected \(expectedData), got \(receivedData) instead", file: file, line: line)
+                
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, "Expected \(expectedError), got \(receivedError) instead", file: file, line: line)
+                
+            default:
+                XCTFail("Expected to receive \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+}
+
+protocol PetImageDataLoaderTestCase: XCTestCase {}
+
+extension PetImageDataLoaderTestCase {
+    func expect(_ sut: PetImageDataLoader, toCompleteWith expectedResult: PetImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         let exp = expectation(description: "Wait for completion")
         _ = sut.loadImageData(from: anyURL()) { receivedResult in
             switch (receivedResult, expectedResult) {
