3ecb752ea5bc7e6c376647d4ed10b28ab318a63a
Wayne <h88377@gmail.com>
2023-04-28 17:23:20 +0800

    Create empty `PetImageDataLoaderWithCacheDecoratorTests`

---
+++ FindYourOnlysClone/FindYourOnlysCloneTests/App/PetImageDataLoaderWithCacheTests.swift
@@ -0,0 +1,12 @@
+//
+//  PetImageDataLoaderWithCacheTests.swift
+//  FindYourOnlysCloneTests
+//
+//  Created by 鄭昭韋 on 2023/4/28.
+//
+
+import XCTest
+
+class PetImageDataLoaderWithCacheDecoratorTests: XCTestCase {
+    
+}
