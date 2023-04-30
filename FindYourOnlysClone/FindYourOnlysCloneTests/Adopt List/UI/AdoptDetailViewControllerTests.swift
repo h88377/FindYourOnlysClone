6874391f8143c412b43bb61424a04c40b3ea90c8
Wayne <h88377@gmail.com>
2023-04-30 22:47:34 +0800

    Create empty `AdoptDetailViewControllerTests` file

---
+++ FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/UI/AdoptDetailViewControllerTests.swift
@@ -0,0 +1,12 @@
+//
+//  AdoptDetailViewControllerTests.swift
+//  FindYourOnlysCloneTests
+//
+//  Created by 鄭昭韋 on 2023/4/30.
+//
+
+import XCTest
+
+class AdoptDetailViewControllerTests: XCTestCase {
+    
+}
