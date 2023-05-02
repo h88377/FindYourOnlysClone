20fe22d191ac78dc2f968fe9861bcf42cbb8279a
Wayne <h88377@gmail.com>
2023-05-02 21:14:19 +0800

    Move `AdoptDetailViewController` test helpers into a separate file

--- FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/UI/AdoptDetailUIIntegrationTests.swift
+++ FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/UI/AdoptDetailUIIntegrationTests.swift
@@ -128,28 +128,3 @@ class AdoptDetailUIIntegrationTests: XCTestCase {
         return pet
     }
 }
-
-extension AdoptDetailViewController {
-    var statusSection: Int { return 0 }
-    var mainInfoSection: Int { return 1 }
-    var infoSection: Int { return 2 }
-    
-    var dataSourceIsConfigured: Bool {
-        return collectionView.dataSource != nil
-    }
-    
-    var headerImage: UIImage? {
-        let header = collectionView.dataSource?.collectionView?(collectionView, viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionHeader, at: IndexPath(item: 0, section: 0)) as? AdoptDetailHeaderView
-        return header?.imageView.image
-    }
-    
-    @discardableResult
-    func simulatePetInfoIsVisibleAt(indexPath: IndexPath) -> UICollectionViewCell? {
-        let dataSource = collectionView.dataSource
-        return dataSource?.collectionView(collectionView, cellForItemAt: indexPath)
-    }
-    
-    func simulateHeaderIsVisible() {
-        simulatePetInfoIsVisibleAt(indexPath: IndexPath(item: 0, section: statusSection))
-    }
-}
