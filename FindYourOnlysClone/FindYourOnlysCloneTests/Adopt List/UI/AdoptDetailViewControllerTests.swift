48293cda4c9110264584d1a577309f25ac65f258
Wayne <h88377@gmail.com>
2023-05-02 14:18:49 +0800

    `AdoptDetailViewController` renders pet information when cell is visible

--- FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/UI/AdoptDetailViewControllerTests.swift
+++ FindYourOnlysClone/FindYourOnlysCloneTests/Adopt List/UI/AdoptDetailViewControllerTests.swift
@@ -8,6 +8,175 @@
 import XCTest
 @testable import FindYourOnlysClone
 
+class AdoptDetailViewModel {
+    private let pet: Pet
+    
+    init(pet: Pet) {
+        self.pet = pet
+    }
+    
+    var statusText: String {
+        switch pet.status {
+        case PetStatusType.open.rawValue:
+            return "開放認養"
+            
+        case PetStatusType.close.rawValue:
+            return "不開放認養"
+            
+        default:
+            return "無"
+        }
+    }
+    
+    var kindText: String {
+        return pet.kind
+    }
+    
+    var genderText: String {
+        switch pet.gender {
+        case PetGenderType.male.rawValue:
+            return "♂"
+            
+        case PetGenderType.female.rawValue:
+            return "♀"
+            
+        default:
+            return "無"
+        }
+    }
+    
+    var varietyText: String {
+        return pet.variety
+    }
+    
+    var idText: String {
+        return String(pet.id)
+    }
+    
+    var ageText: String {
+        switch pet.age {
+        case PetAgeType.adult.rawValue:
+            return "成年"
+            
+        case PetAgeType.child.rawValue:
+            return "幼年"
+            
+        default:
+            return "無"
+        }
+    }
+    
+    var colorText: String {
+        return pet.color
+    }
+    
+    var bodyTypeText: String {
+        switch pet.bodyType {
+        case PetBodyType.small.rawValue:
+            return "小型"
+            
+        case PetBodyType.medium.rawValue:
+            return "中型"
+            
+        case PetBodyType.big.rawValue:
+            return "大型"
+            
+        default:
+            return "無"
+        }
+    }
+    
+    var foundPlaceText: String {
+        return pet.foundPlace
+    }
+    
+    var sterilizationText: String {
+        switch pet.sterilization {
+        case PetBoolStatus.beTrue.rawValue:
+            return "是"
+            
+        case PetBoolStatus.beFalse.rawValue:
+            return "否"
+            
+        default:
+            return "無"
+        }
+    }
+    
+    var bacterinText: String {
+        switch pet.bacterin {
+        case PetBoolStatus.beTrue.rawValue:
+            return "是"
+            
+        case PetBoolStatus.beFalse.rawValue:
+            return "否"
+            
+        default:
+            return "無"
+        }
+    }
+    
+    var openForAdoptionDateText: String {
+        return pet.openDate == "" ? "無" : pet.openDate
+    }
+    
+    var closeForAdoptionDateText: String {
+        return pet.closedDate == "" ? "無" : pet.closedDate
+    }
+    
+    var updatedDateText: String {
+        return pet.updatedDate == "" ? "無" : pet.updatedDate
+    }
+    
+    var createdDateText: String {
+        return pet.createdDate == "" ? "無" : pet.createdDate
+    }
+    
+    var shelterNameText: String {
+        return pet.shelterName
+    }
+    
+    var addressText: String {
+        return pet.address
+    }
+    
+    var telephoneText: String {
+        return pet.telephone
+    }
+    
+    var remarkText: String {
+        return pet.remark == "" ? "無" : pet.remark
+    }
+}
+
+extension AdoptDetailViewModel {
+    enum PetStatusType: String {
+        case open = "OPEN"
+        case close = "CLOSE"
+    }
+    
+    enum PetGenderType: String {
+        case male = "M"
+        case female = "F"
+    }
+    
+    enum PetBodyType: String {
+        case small = "SMALL"
+        case medium = "MEDIUM"
+        case big = "BIG"
+    }
+    
+    enum PetAgeType: String {
+        case adult = "ADULT"
+        case child = "CHILD"
+    }
+    
+    enum PetBoolStatus: String {
+        case beTrue = "T"
+        case beFalse = "F"
+    }
+}
+
 final class AdoptDetailStatusCell: UICollectionViewCell {
     let statusLabel = UILabel()
 }
@@ -26,11 +195,16 @@ final class AdoptDetailViewController: UIViewController {
     let collectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
         
+        collectionView.register(AdoptDetailStatusCell.self, forCellWithReuseIdentifier: AdoptDetailStatusCell.identifier)
+        collectionView.register(AdoptDetailMainInfoCell.self, forCellWithReuseIdentifier: AdoptDetailMainInfoCell.identifier)
+        collectionView.register(AdoptDetailInfoCell.self, forCellWithReuseIdentifier: AdoptDetailInfoCell.identifier)
+        
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
     }()
     
     private let sections = AdoptDetailSection.allCases
+    private let viewModel: AdoptDetailViewModel
     
     private lazy var dataSource: UICollectionViewDiffableDataSource<AdoptDetailSection, AdoptDetailModel> = {
         .init(collectionView: collectionView) { [weak self] collectionView, indexPath, model in
@@ -40,17 +214,71 @@ final class AdoptDetailViewController: UIViewController {
         }
     }()
     
+    init(viewModel: AdoptDetailViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.dataSource = self.dataSource
+        configureSnapshot()
+    }
+    
+    private func configureSnapshot() {
+        var snapshot = dataSource.snapshot()
+        snapshot.appendSections(sections)
+        
+        let statusModel = [
+            AdoptDetailModel(description: viewModel.statusText)
+        ]
+        snapshot.appendItems(statusModel, toSection: .status)
+        
+        let mainModels = [
+            AdoptDetailModel(title: MainInfoSection.kind.rawValue, description: viewModel.kindText),
+            AdoptDetailModel(title: MainInfoSection.gender.rawValue, description: viewModel.genderText),
+            AdoptDetailModel(title: MainInfoSection.variety.rawValue, description: viewModel.varietyText)
+        ]
+        snapshot.appendItems(mainModels, toSection: .mainInfo)
+        
+        let infoModels = [
+            AdoptDetailModel(title: InfoSection.id.rawValue, description: viewModel.idText),
+            AdoptDetailModel(title: InfoSection.age.rawValue, description: viewModel.ageText),
+            AdoptDetailModel(title: InfoSection.color.rawValue, description: viewModel.colorText),
+            AdoptDetailModel(title: InfoSection.bodyType.rawValue, description: viewModel.bodyTypeText),
+            AdoptDetailModel(title: InfoSection.foundPlace.rawValue, description: viewModel.foundPlaceText),
+            AdoptDetailModel(title: InfoSection.sterilization.rawValue, description: viewModel.sterilizationText),
+            AdoptDetailModel(title: InfoSection.bacterin.rawValue, description: viewModel.bacterinText),
+            AdoptDetailModel(title: InfoSection.openDate.rawValue, description: viewModel.openForAdoptionDateText),
+            AdoptDetailModel(title: InfoSection.closedDate.rawValue, description: viewModel.closeForAdoptionDateText),
+            AdoptDetailModel(title: InfoSection.updatedDate.rawValue, description: viewModel.updatedDateText),
+            AdoptDetailModel(title: InfoSection.createdDate.rawValue, description: viewModel.createdDateText),
+            AdoptDetailModel(title: InfoSection.shelterName.rawValue, description: viewModel.shelterNameText),
+            AdoptDetailModel(title: InfoSection.address.rawValue, description: viewModel.addressText),
+            AdoptDetailModel(title: InfoSection.telephone.rawValue, description: viewModel.telephoneText),
+            AdoptDetailModel(title: InfoSection.remark.rawValue, description: viewModel.remarkText),
+        ]
+        snapshot.appendItems(infoModels, toSection: .info)
+        
+        // apply
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
 }
 
-private extension AdoptDetailViewController {
+extension AdoptDetailViewController {
     struct AdoptDetailModel: Hashable {
-        let title: String
+        let title: String?
         let description: String
+        
+        init(title: String? = nil, description: String) {
+            self.title = title
+            self.description = description
+        }
     }
     
     enum AdoptDetailSection: Hashable, CaseIterable {
@@ -77,13 +305,13 @@ private extension AdoptDetailViewController {
                 let cell: AdoptDetailStatusCell = collectionView.dequeueReusableCell(for: indexPath)
                 cell.statusLabel.text = model.description
                 return cell
-
+                
             case .mainInfo:
                 let cell: AdoptDetailMainInfoCell = collectionView.dequeueReusableCell(for: indexPath)
                 cell.infoTitleLabel.text = model.title
                 cell.infoLabel.text = model.description
                 return cell
-
+                
             case .info:
                 let cell: AdoptDetailInfoCell = collectionView.dequeueReusableCell(for: indexPath)
                 cell.infoTitleLabel.text = model.title
@@ -91,6 +319,30 @@ private extension AdoptDetailViewController {
                 return cell
             }
         }
+    }
+    
+    enum MainInfoSection: String, CaseIterable {
+        case kind = "種類"
+        case gender = "性別"
+        case variety = "品種"
+    }
+    
+    enum InfoSection: String, CaseIterable {
+        case id = "動物流水編號"
+        case age = "動物年齡"
+        case color = "動物毛色"
+        case bodyType = "動物體型"
+        case foundPlace = "尋獲地點"
+        case sterilization = "是否節育"
+        case bacterin = "是否打狂犬疫苗"
+        case openDate = "開放領養時間"
+        case closedDate = "截止領養時間"
+        case updatedDate = "資料更新時間"
+        case createdDate = "資料建立時間"
+        case shelterName = "領養機構"
+        case address = "領養地址"
+        case telephone = "領養電話"
+        case remark = "備註"
     }
 }
 
@@ -103,17 +355,159 @@ class AdoptDetailViewControllerTests: XCTestCase {
         XCTAssertTrue(sut.dataSourceIsConfigured)
     }
     
+    func test_cellIsVisible_rendersPetInformation() {
+        let pet = makePet()
+        let viewModel = AdoptDetailViewModel(pet: pet)
+        let sut = AdoptDetailViewController(viewModel: viewModel)
+
+        sut.loadViewIfNeeded()
+        
+        expect(sut, isRenderingWhenCellIsVisible: viewModel)
+    }
+    
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> AdoptDetailViewController {
-        let sut = AdoptDetailViewController()
+    private func makeSUT(pet: Pet = Pet(id: 0, location: "新北市XX", kind: "貓", gender: "F", bodyType: "SMALL", color: "黑", age: "CHILD", sterilization: "T", bacterin: "F", foundPlace: "新北市FoundPlace", status: "OPEN", remark: "", openDate: "2023-04-22", closedDate: "2023-04-22", updatedDate: "2023-04-22", createdDate: "2023-04-22", photoURL: nil, address: "新北市XXX", telephone: "02-XXXXXXXX", variety: "混種", shelterName: "新北市收容所"), image: UIImage? = nil, file: StaticString = #filePath, line: UInt = #line) -> AdoptDetailViewController {
+        let viewModel = AdoptDetailViewModel(pet: pet)
+        let sut = AdoptDetailViewController(viewModel: viewModel)
         trackForMemoryLeak(sut, file: file, line: line)
+        trackForMemoryLeak(viewModel, file: file, line: line)
         return sut
+    }
+    
+    private func expect(_ sut: AdoptDetailViewController, isRenderingWhenCellIsVisible viewModel: AdoptDetailViewModel, file: StaticString = #filePath, line: UInt = #line) {
+        let view = sut.simulatePetInfoIsVisibleAt(indexPath: IndexPath(item: 0, section: sut.statusSection))
+        guard let statusCell = view as? AdoptDetailStatusCell else {
+            return XCTFail("Expected cell instance \(AdoptDetailStatusCell.self), got \(String(describing: view.self)) instance instead", file: file, line: line)
+        }
+        XCTAssertEqual(statusCell.statusLabel.text, viewModel.statusText, "Expected status should be \(viewModel.statusText)", file: file, line: line)
+        
+        for (index, mainInfo) in AdoptDetailViewController.MainInfoSection.allCases.enumerated() {
+            expect(sut, hasMainInfoViewConfiuredFor: viewModel, mainInfo: mainInfo, at: index, file: file, line: line)
+        }
+        
+        for (index, info) in AdoptDetailViewController.InfoSection.allCases.enumerated() {
+            expect(sut, hasInfoViewConfiuredFor: viewModel, info: info, at: index, file: file, line: line)
+        }
+    }
+    
+    private func expect(_ sut: AdoptDetailViewController, hasMainInfoViewConfiuredFor viewModel: AdoptDetailViewModel, mainInfo: AdoptDetailViewController.MainInfoSection, at index: Int, file: StaticString = #filePath, line: UInt = #line) {
+        let view = sut.simulatePetInfoIsVisibleAt(indexPath: IndexPath(item: index, section: sut.mainInfoSection))
+        guard let cell = view as? AdoptDetailMainInfoCell else {
+            return XCTFail("Expected cell instance \(AdoptDetailMainInfoCell.self), got \(String(describing: view.self)) instance instead", file: file, line: line)
+        }
+        
+        let viewModelText: String
+        switch mainInfo {
+        case .kind:
+            viewModelText = viewModel.kindText
+            
+        case .gender:
+            viewModelText = viewModel.genderText
+            
+        case .variety:
+            viewModelText = viewModel.varietyText
+        }
+        
+        XCTAssertEqual(cell.infoTitleLabel.text, mainInfo.rawValue, "Expected \(mainInfo) title text should be  \(mainInfo.rawValue)", file: file, line: line)
+        XCTAssertEqual(cell.infoLabel.text, viewModelText, "Expected \(mainInfo) info text should be  \(viewModelText)", file: file, line: line)
+    }
+    
+    private func expect(_ sut: AdoptDetailViewController, hasInfoViewConfiuredFor viewModel: AdoptDetailViewModel, info: AdoptDetailViewController.InfoSection, at index: Int, file: StaticString = #filePath, line: UInt = #line) {
+        let view = sut.simulatePetInfoIsVisibleAt(indexPath: IndexPath(item: index, section: sut.infoSection))
+        guard let cell = view as? AdoptDetailInfoCell else {
+            return XCTFail("Expected cell instance \(AdoptDetailInfoCell.self), got \(String(describing: view.self)) instance instead", file: file, line: line)
+        }
+        
+        let viewModelText: String
+        switch info {
+        case .id:
+            viewModelText = viewModel.idText
+            
+        case .age:
+            viewModelText = viewModel.ageText
+            
+        case .color:
+            viewModelText = viewModel.colorText
+            
+        case .bodyType:
+            viewModelText = viewModel.bodyTypeText
+            
+        case .foundPlace:
+            viewModelText = viewModel.foundPlaceText
+            
+        case .sterilization:
+            viewModelText = viewModel.sterilizationText
+            
+        case .bacterin:
+            viewModelText = viewModel.bacterinText
+            
+        case .openDate:
+            viewModelText = viewModel.openForAdoptionDateText
+            
+        case .closedDate:
+            viewModelText = viewModel.closeForAdoptionDateText
+            
+        case .updatedDate:
+            viewModelText = viewModel.updatedDateText
+            
+        case .createdDate:
+            viewModelText = viewModel.createdDateText
+        case .shelterName:
+            viewModelText = viewModel.shelterNameText
+            
+        case .address:
+            viewModelText = viewModel.addressText
+            
+        case .telephone:
+            viewModelText = viewModel.telephoneText
+            
+        case .remark:
+            viewModelText = viewModel.remarkText
+        }
+        
+        XCTAssertEqual(cell.infoTitleLabel.text, info.rawValue, "Expected \(info) title text should be  \(info.rawValue)", file: file, line: line)
+        XCTAssertEqual(cell.infoLabel.text, viewModelText, "Expected \(info) info text should be  \(viewModelText)", file: file, line: line)
+    }
+    
+    private func makePet(id: Int = 0, location: String = "新北市XX", kind: String = "貓", gender: String = "F", bodyType: String = "SMALL", color: String = "黑", age: String = "CHILD", sterilization: String = "T", bacterin: String = "F", foundPlace: String = "新北市FoundPlace", status: String = "OPEN", remark: String = "", openDate: String = "2023-04-22", closedDate: String = "2023-04-22", updatedDate: String = "2023-04-22", createdDate: String = "2023-04-22", photoURL: URL? = nil, address: String = "新北市XXX", telephone: String = "02-XXXXXXXX", variety: String = "混種", shelterName: String = "新北市收容所") -> Pet {
+        let pet = Pet(
+            id: id,
+            location: location,
+            kind: kind,
+            gender: gender,
+            bodyType: bodyType,
+            color: color,
+            age: age,
+            sterilization: sterilization,
+            bacterin: bacterin,
+            foundPlace: foundPlace,
+            status: status,
+            remark: remark,
+            openDate: openDate,
+            closedDate: closedDate,
+            updatedDate: updatedDate,
+            createdDate: createdDate,
+            photoURL: photoURL,
+            address: address,
+            telephone: telephone,
+            variety: variety,
+            shelterName: shelterName)
+        return pet
     }
 }
 
 extension AdoptDetailViewController {
+    var statusSection: Int { return 0 }
+    var mainInfoSection: Int { return 1 }
+    var infoSection: Int { return 2 }
+    
     var dataSourceIsConfigured: Bool {
         return collectionView.dataSource != nil
+    }
+    
+    func simulatePetInfoIsVisibleAt(indexPath: IndexPath) -> UICollectionViewCell? {
+        let dataSource = collectionView.dataSource
+        return dataSource?.collectionView(collectionView, cellForItemAt: indexPath)
     }
 }
