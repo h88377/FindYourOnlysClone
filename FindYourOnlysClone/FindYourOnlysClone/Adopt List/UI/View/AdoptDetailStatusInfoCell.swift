1f9cc29734b33a65ea22ff03d3bd9037cf1531fd
Wayne <h88377@gmail.com>
2023-05-03 11:43:40 +0800

    Configure `AdoptDetailStatusInfoCell` UI layout

--- FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/View/AdoptDetailStatusInfoCell.swift
+++ FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/View/AdoptDetailStatusInfoCell.swift
@@ -8,5 +8,34 @@
 import UIKit
 
 final class AdoptDetailStatusInfoCell: UICollectionViewCell {
-    let statusLabel = UILabel()
+    let statusLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        label.textColor = .darkGray
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        contentView.addSubview(statusLabel)
+        
+        NSLayoutConstraint.activate([
+            statusLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            statusLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            statusLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 21)
+        ])
+    }
 }
