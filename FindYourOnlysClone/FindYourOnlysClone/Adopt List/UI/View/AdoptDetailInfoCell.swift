468fc9de98137462d9e31a0d3b6e3d1fa2641b7b
Wayne <h88377@gmail.com>
2023-05-03 11:43:08 +0800

    Configure `AdoptDetailInfoCell` UI layout

--- FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/View/AdoptDetailInfoCell.swift
+++ FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/View/AdoptDetailInfoCell.swift
@@ -8,6 +8,47 @@
 import UIKit
 
 final class AdoptDetailInfoCell: UICollectionViewCell {
-    let infoTitleLabel = UILabel()
-    let infoLabel = UILabel()
+    let infoTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .darkGray
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .darkGray
+        label.textAlignment = .right
+        label.numberOfLines = 0
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        contentView.addSubviews([infoTitleLabel, infoLabel])
+        
+        NSLayoutConstraint.activate([
+            infoTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            infoTitleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            infoTitleLabel.trailingAnchor.constraint(lessThanOrEqualTo: infoLabel.leadingAnchor, constant: -10),
+            
+            infoLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            infoLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            infoLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10)
+        ])
+    }
 }
