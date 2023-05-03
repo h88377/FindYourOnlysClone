6b27b07bc5b2faa62f28498c128b91efb50da236
Wayne <h88377@gmail.com>
2023-05-03 11:42:49 +0800

    Configure `AdoptDetailHeaderView` UI layout

--- FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/View/AdoptDetailHeaderView.swift
+++ FindYourOnlysClone/FindYourOnlysClone/Adopt List/UI/View/AdoptDetailHeaderView.swift
@@ -8,6 +8,34 @@
 import UIKit
 
 final class AdoptDetailHeaderView: UICollectionReusableView {
-    static let identifier = "AdoptDetailHeaderView"
-    let imageView = UIImageView()
+    static let identifier = "\(AdoptDetailHeaderView.self)"
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
 }
